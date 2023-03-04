52b291b5ba27950b42e48cbd4ac8d6c4acc79283
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-03-03 21:43:42 -0300

    Hello World!

---
+++ wolfbroker/src/main/java/com/mycompany/wolfbroker/Wolfbroker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wolfbroker;
+
+/**
+ *
+ * @author tiago.fribeiro
+ */
+public class Wolfbroker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
