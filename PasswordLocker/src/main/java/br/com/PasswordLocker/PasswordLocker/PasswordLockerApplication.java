125db4a24728fed95c0ab92f1c74b3ae86a819d3
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2023-03-17 21:31:09 -0300

    Adicionando estrutura da API rest

---
+++ PasswordLocker/src/main/java/br/com/PasswordLocker/PasswordLocker/PasswordLockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.PasswordLocker.PasswordLocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordLockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordLockerApplication.class, args);
+	}
+
+}
