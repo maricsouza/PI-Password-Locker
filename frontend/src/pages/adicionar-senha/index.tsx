815e9bcc3ba2b74d32ba2ea682689de2ff858801
KevinAlvss <kevin.bernardo64@gmail.com>
2023-05-20 11:39:46 -0300

    feat: adding generate password component

--- frontend/src/pages/adicionar-senha/index.tsx
+++ frontend/src/pages/adicionar-senha/index.tsx
@@ -1,59 +1,39 @@
-import Header from "@/components/header"
-import style from "./style.module.scss"
-import FullInput from "@/components/full-input"
-import { buttonTypes, sizes } from "@/styles/global.type"
-import ButtonType from "@/components/Button"
-import Card from "@/components/card"
-import Button from "@/components/Button"
+import Header from "@/components/header";
+import style from "./style.module.scss";
+import FullInput from "@/components/full-input";
+import Card from "@/components/card";
+import GeneratePassword from "@/components/GeneratePassword";
 
-export default function AdicionarSenha () {
-    return (
-        <div className={style.container}>
-            <Header title="Adicionar Senha" returnPage='/dashboard' />
+export default function AdicionarSenha() {
+  return (
+    <div className={style.container}>
+      <Header title="Adicionar Senha" returnPage="/dashboard" />
 
-            <div className={style.principalBox}>
-                <div className={style.infoContainer}> 
-                    <div>
-                        <h1> Dados da senha </h1>
-                        <hr />
+      <div className={style.principalBox}>
+        <div className={style.infoContainer}>
+          <div>
+            <h1> Dados da senha </h1>
+            <hr />
 
-                        <div className={style.box}>
-                            <FullInput
-                                inputTitle="Nome do site"
-                            />
-                            <FullInput
-                                inputTitle="Nome do usuário do site (opcional)"
-                            />
-                        </div>
-
-                        <div className={style.box}>
-                            <FullInput
-                                inputTitle="Nova senha"
-                            />
-                            <FullInput
-                                inputTitle="Confirmar senha"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <h1> Gerar senha forte </h1>
-                        <hr />
-
-                        <div className={style.box2}>
-                            <FullInput/>
-                            <Button backgcolor="#0051D0" fontcolor="#FFF" typeofbutton="textButton" text="Gerar" fontsize="" size={sizes.large} onclick={ () => console.log('ok')}/>
-                        </div>
-                        
-
-                    </div>
-                </div>
-                <Card cardFormat={1} text="Preencha os dados ao lado para criar senha." buttonText="Adicionar" />
+            <div className={style.box}>
+              <FullInput inputTitle="Nome do site" />
+              <FullInput inputTitle="Nome do usuário do site (opcional)" />
             </div>
-            
 
-           
-            
+            <div className={style.box}>
+              <FullInput inputTitle="Nova senha" />
+              <FullInput inputTitle="Confirmar senha" />
+            </div>
+          </div>
+
+          <GeneratePassword />
         </div>
-    )
+        <Card
+          cardFormat={1}
+          text="Preencha os dados ao lado para criar senha."
+          buttonText="Adicionar"
+        />
+      </div>
+    </div>
+  );
 }
