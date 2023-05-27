ee28fa4043edad83a4508eb14ab055e16b5f63c2
KevinAlvss <kevin.bernardo64@gmail.com>
2023-05-26 21:44:01 -0300

    feat: add validations in create account

--- frontend/src/pages/sign-up/index.tsx
+++ frontend/src/pages/sign-up/index.tsx
@@ -6,37 +6,47 @@ import { Account } from "@/services/endpoints/account";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 
-const api = new Account()
+const api = new Account();
 
 export default function SignUp() {
-  const router = useRouter()
+  const router = useRouter();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
-  const [confirmedPassword, setConfirmedPassword] = useState("")
-  const [phoneNumber, setPhoneNumber] = useState("")
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-  async function signUp (name: string, email: string, pass: string, confirmPass: string, phone: string) {    
+  async function signUp(
+    name: string,
+    email: string,
+    pass: string,
+    confirmPass: string,
+    phone: string
+  ) {
     try {
       if (!name || !email || !pass || !confirmPass || !phone) {
         throw new Error("Todos os campos precisam ser preenchidos");
       }
 
-      if(!pass.match(confirmPass)) {
-        throw new Error("senhas não batem"); 
+      if (!pass.match(confirmPass)) {
+        throw new Error("senhas não batem");
       }
-      
-      await api.createAccount({name: name, password: pass, email: email, phoneNumber: phone, interf:"account"});
-      
-      toast.success('Conta criada com sucesso')
-      router.push('/login')
 
-    } catch(e: any) {
-      toast.error(e.message)
+      await api.createAccount({
+        name: name,
+        password: pass,
+        email: email,
+        phoneNumber: phone,
+        interf: "account",
+      });
+
+      toast.success("Conta criada com sucesso");
+      router.push("/login");
+    } catch (e: any) {
+      toast.error("Ocorreu um erro ao cadastrar sua conta");
     }
   }
-
 
   return (
     <div className={style.container}>
@@ -45,18 +55,50 @@ export default function SignUp() {
           <ImageWithLogo largura="320px" />
         </div>
         <div className={style.colun1}>
-          <Input content={"Nome "} value={name} onChange={(e) => setName(e.target.value)}/>
-          <Input content={"Telefone"} type="number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          <Input
+            content={"Nome "}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            content={"Telefone"}
+            type="number"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
         </div>
         <div className={style.colun2}>
-          <Input content={"Email"} largura={"820px"} type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <Input
+            content={"Email"}
+            largura={"820px"}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className={style.colun3}>
-          <Input content={"Senha"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <Input content={"Confirmar Senha"} type="password" value={confirmedPassword} onChange={(e) => setConfirmedPassword(e.target.value)}/>
+          <Input
+            content={"Senha"}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            content={"Confirmar Senha"}
+            type="password"
+            value={confirmedPassword}
+            onChange={(e) => setConfirmedPassword(e.target.value)}
+          />
         </div>
         <div className={style.btnCadastrar}>
-          <Button text={"Cadastrar"} size="500px" typeofbutton="textButton" onClick={() => signUp(name, email, password, confirmedPassword, phoneNumber)}/>
+          <Button
+            text={"Cadastrar"}
+            size="500px"
+            typeofbutton="textButton"
+            onClick={() =>
+              signUp(name, email, password, confirmedPassword, phoneNumber)
+            }
+          />
           <div className={style.textEntrar}>
             <text>
               Ja possui conta?
--- frontend/src/services/endpoints/account.ts
+++ frontend/src/services/endpoints/account.ts
@@ -1,47 +1,50 @@
 import { api } from "../extensions/axios";
 
 export class Account {
-
-  //ENDPOINT :: LOGIN => email e senha
-  async login (email: string, password: string) {
-    const r = await api.post<IRAccount>("/usuarios/login", {}, {
-      headers: {
-        usuario: email,
-        senha: password
+  async login(email: string, password: string) {
+    const response = await api.post<IRAccount>(
+      "/usuarios/login",
+      {},
+      {
+        headers: {
+          usuario: email,
+          senha: password,
+        },
       }
-    })
+    );
 
-    return r.data;
+    return response.data;
   }
 
-  // ENDPOINT :: CRIAR CONTA => nome, email, telefone e senha
   async createAccount(data: IAccount) {
-    const r = await api.post("/usuarios", {
+    const response = await api.post("/usuarios", {
       nome: data.name,
       email: data.email,
       senha: data.password,
-      telefone: data.phoneNumber
+      telefone: data.phoneNumber,
     });
 
-    return r.data
+    return response.data;
   }
 
-  // ENDPOINT :: ALTERAR DADOS DA CONTA => id, nome, email, telefone e senha
   async modifyAccountData(data: IAccount) {
-    const r = await api.put("/usuarios", {
+    const response = await api.put("/usuarios", {
       nome: data.name,
       email: data.email,
       senha: data.password,
-      telefone: data.phoneNumber
-
+      telefone: data.phoneNumber,
     });
 
-    return r.data
+    return response.data;
   }
 
-  //ENDPOINT :: EXCLUIR CONTA => id
-  async deleteAccount (id: IAccount) {
-    let r = await api.delete("/usuarios")
-    return r.data;
+  async deleteAccount(data: IAccount) {
+    const response = await api.delete("/usuarios/", {
+      headers: {
+        idUser: data.id,
+      },
+    });
+
+    return response.data;
   }
 }
